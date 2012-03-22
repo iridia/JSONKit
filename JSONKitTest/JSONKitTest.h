34eea4c85e55fdcc603ac1bd2c1928f4f8be6200
Evadne Wu <ev@monoceroi.com>
2012-03-23 03:13:43 +0800

    adds a simple stub for retroactively coding various (fixed) failure cases

---
+++ JSONKitTest/JSONKitTest.h
@@ -0,0 +1,12 @@
+//
+//  JSONKit.h
+//  http://github.com/waveface/JSONKit
+//  Dual licensed under either the terms of the BSD License, or alternatively
+//  under the terms of the Apache License, Version 2.0, as specified below.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface JSONKitTest : SenTestCase
+
+@end
